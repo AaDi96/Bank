da792b965b0e5c688d2937b84f885ed2a5ca7b43
AaDi <aadisarwar96@gmail.com>
2018-05-21 16:21:48 +0500

    Ahtasham

---
+++ Exam/Exam/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exam.Models;
+namespace Exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private dbContext db = new dbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var list = db.Students.ToList();
+            return View(list);
+        }
+    public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student S)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(S);
+                db.SaveChanges();
+                return Redirect(Index);
+            }
+            return View(S);
+            
+        }
+       
+    }
+}
---
+++ Exam/Exam/Models/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Exam.Models
+{
+    public class dbContext:DbContext
+
+    {
+        public dbContext() : base("DB")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
